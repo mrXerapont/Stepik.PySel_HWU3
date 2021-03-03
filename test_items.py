3fc888933dcd055f67568b65850897a272c5551a
mrXerapont <mrtalstyak@gmail.com>
2021-03-03 22:45:42 +0400

    test_items.py edited (time.sleep 30 now)

--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ def test_add_to_card_btn_should_exist(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.implicitly_wait(10)
     browser.get(link)
+    time.sleep(30)
 
     # Не знаю какой вариант оставить, оставлю этот тут для себя
     # btn_pre = browser.find_elements_by_css_selector("#add_to_basket_form .btn-add-to-basket")
@@ -16,6 +17,5 @@ def test_add_to_card_btn_should_exist(browser):
     except NoSuchElementException:
         btn_present = False
 
-    time.sleep(10)
     assert btn_present, "Error! 'Add to basket' button not found!"
     # assert len(btn_pre) > 0, "Error! 'Add to basket' button not found!"
